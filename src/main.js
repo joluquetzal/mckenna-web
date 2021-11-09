2b3ebca570ada35b914db0761da4a46583ad1f0f
jose luis <jluissalber@gmail.com>
2021-11-09 17:45:15 -0600

    first configuration

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import DefaultLayout from './layouts/DefaultLayout';
+
+import Flare from '@lkmx/flare';
+
 Vue.config.productionTip = false
+
+Vue.use(Flare);
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.components('DefaultLayout', DefaultLayout);
