2ed94d220bbf459e9bb3e513c51c5029247f5af0
jose luis saldivar <joseluis.saldivar@lkmx.io>
2021-11-09 18:24:14 -0600

    adds components

---
+++ src/components/Description.vue
@@ -0,0 +1,45 @@
+<templae>
+  <single-column-layout>
+    <block>
+      <div class="home">
+        <h1 class="title">MckennaCorp</h1>
+        <p class="description">Soluciones que damos para proteger a la empresa y sus clientes</p>
+      </div>
+    </block>
+  </single-column-layout>
+</template>
+
+<script>
+import SingleColumnLayout from '../layouts/SingleColumnLayout.vue';
+
+export default {
+    name: 'Work',
+    components: { SingleColumnLayout},
+}
+</script>
+
+<style lang="scss"
+        TripleColumnLayout scoped>
+.home {
+  padding-top: 100px;
+  padding-bottom: 100px;
+  text-align: center;
+  .title {
+      font-size: 155px;
+  }
+  .description {
+      font-size: 20px;
+      font-weight: 500;
+  }
+}
+
+@include respond-to("medium and down") {
+  .home {
+    padding-top: unset;
+    .title {
+        font-size: 55px;
+    }
+  }
+}
+</style>
+
---
+++ src/components/Work.vue
@@ -0,0 +1,96 @@
+<template>
+  <triple-column-layout>
+    <block>
+      <div class="home">
+        <div class="content">
+          <h1>Mantenimiento</h1>
+    
+          <p>
+            Creamos propuestas de control ambiental y de cuidado de
+            los ductos que se encuentren en los restaurantes, 
+            con una gran variedad de opciones y planes que se les pueden
+            otorgar para dar solucion y mantener el area a salvo de incendios.
+          </p>
+          
+          <button>Conocer mas</button>
+        </div>
+      </div>
+    </block>
+    <block>
+      <div class="description">
+        <div class="content">
+          <h1>Diseño de ductos</h1>
+      
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+            ullamcorper urna vitae posuere feugiat. Sed at purus eget metus ultrices 
+            interdum. In hac habitasse platea dictumst. Donec quis facilisis enim. 
+            Vestibulum faucibus sagittis massa, sed auctor nibh varius at. Etiam lorem odio, 
+            lobortis vel semper et, varius vel diam. 
+            Proin sed tortor sapien. Phasellus mollis hendrerit augue quis tincidunt. 
+          </p>
+          
+          <button>Conocer mas</button>
+        </div>
+      </div>
+    </block>
+    <block>
+      <div class="des">
+        <div class="content">
+          <h1>Servicios extra</h1>
+      
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+            ullamcorper urna vitae posuere feugiat. Sed at purus eget metus ultrices 
+            interdum. In hac habitasse platea dictumst. Donec quis facilisis enim. 
+            Vestibulum faucibus sagittis massa, sed auctor nibh varius at.
+          </p>
+          
+          <button>Conocer mas</button>
+        </div>
+      </div>
+    </block>
+  </triple-column-layout>
+</template>
+
+<script>
+import TripleColumnLayout from '../layouts/TripleColumnLayout.vue';
+
+export default {
+    name: 'Work',
+    components: { TripleColumnLayout},
+}
+</script>
+
+<style lang="scss"
+        TripleColumnLayout scoped>
+.home {
+  height: 550px;
+  text-align: center;
+  background: aliceblue;
+  .content {
+    padding-top: 30%;
+  }
+}
+.description {
+  height: 550px;
+  text-align: center;
+  background: honeydew;
+  .content {
+    padding-top: 30%;
+  }
+}
+.des {
+  height: 550px;
+  text-align: center;
+  background: whitesmoke;
+  .content {
+    padding-top: 30%;
+  }
+}
+@include respond-to("medium and down") {
+  .home {
+    padding-top: unset;
+  }
+}
+</style>
