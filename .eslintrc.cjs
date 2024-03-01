d3efd8ba4361693843002e8c50a2bfe4da9167a3
joluquetzal <jluissalber@gmail.com>
2024-03-01 03:57:07 +0100

    adds/prune dependecies

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
